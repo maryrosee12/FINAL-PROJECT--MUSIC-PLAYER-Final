67d94d935f52fea41e7b88ba44c6fb29c97bbde1
Maryrose R.  Coriño <corinomaryrose00@gmail.com>
2025-12-01 22:21:12 +0800

    Update SongAdapter.java

--- app/src/main/java/com/maryrosee12/tmplayer/ui/library/SongAdapter.java
+++ app/src/main/java/com/maryrosee12/tmplayer/ui/library/SongAdapter.java
@@ -1,4 +1,4 @@
-package com.adepge.tmplayer.ui.library;
+package com.example.musicplayer.ui.library;
 
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -8,31 +8,21 @@ import android.widget.ImageView;
 import android.widget.TextView;
 import androidx.recyclerview.widget.RecyclerView;
 
-import com.adepge.tmplayer.R;
+import com.example.musicplayer.R;
 import com.bumptech.glide.Glide;
 import java.util.List;
 
-/**
- * This adapter generates child Views from the data source (song list) to populate
- * the RecyclerView in {@link LibraryFragment}
- *
- * @version 1.0
- * @author Adam George
- */
 public class SongAdapter extends RecyclerView.Adapter<SongAdapter.SongViewHolder> {
 
-    // Interface for ClickListener on a song
     public interface OnSongClickListener {
         void onSongClick(Song song);
     }
 
-    // Static class for child ViewHolder
     public static class SongViewHolder extends RecyclerView.ViewHolder {
         ImageView albumCover;
         TextView title;
         TextView artist;
 
-        // Initialise view resources
         public SongViewHolder(View v) {
             super(v);
             albumCover = v.findViewById(R.id.album_thumbnail);
@@ -44,34 +34,17 @@ public class SongAdapter extends RecyclerView.Adapter<SongAdapter.SongViewHolder
     private List<Song> songs;
     private OnSongClickListener listener;
 
-    // Constructor
     public SongAdapter(List<Song> songs, OnSongClickListener listener) {
         this.songs = songs;
         this.listener = listener;
     }
 
-    /**
-     * Creates the ViewHolder
-     *
-     * @param parent The ViewGroup into which the new View will be added after it is bound to
-     *               an adapter position.
-     * @param viewType The view type of the new View.
-     *
-     * @return the SongViewHolder (album art, song title, artist)
-     */
     @Override
     public SongViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
         View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.song_item, parent, false);
         return new SongViewHolder(v);
     }
 
-    /**
-     * Load data into ViewHolder after binding adapter to the RecyclerView
-     *
-     * @param holder The ViewHolder which should be updated to represent the contents of the
-     *        item at the given position in the data set.
-     * @param position The position of the item within the adapter's data set.
-     */
     @Override
     public void onBindViewHolder(SongViewHolder holder, int position) {
         Song song = songs.get(position);
@@ -93,12 +66,9 @@ public class SongAdapter extends RecyclerView.Adapter<SongAdapter.SongViewHolder
         });
     }
 
-    /**
-     * Getter method
-     * @return the number of songs in the SongList
-     */
     @Override
     public int getItemCount() {
         return songs.size();
     }
 }
+
